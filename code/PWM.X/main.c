fdd4033798ad85e1c0dc5e957b47931195111db9
Cillian O'Brien <cillianobrien01@gmail.com>
2021-02-09 23:03:09 +0000

    working 50% PWM on RB6

--- code/PWM.X/main.c
+++ code/PWM.X/main.c
@@ -14,24 +14,25 @@
 #pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit 
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit 
 
+#define DUTY 50
 #define _XTAL_FREQ 4000000
 #define TMR2Prescale 1          // TMR2 Prescale value
-uint16_t PWMFreq = 5000;            // PWM Frequency
+uint16_t PWMFreq = 440;         // PWM Frequency
 
 void PWM_Init(void)
 {
     TRISBbits.TRISB6 = 1;                   // Set RB6 as input 
     RB6PPSbits.RB6PPS = 0b001101;           // set PWM 6 to output on RB6 
-    PWM7CON = 0;                            // Clear PWM7CON register
+    PWM6CON = 0;                            // Clear PWM7CON register
     T2PRbits.PR = (_XTAL_FREQ/(PWMFreq*4*TMR2Prescale))-1;  // PWM period
-    PWM6DC = T2PRbits.PR / 2;       // Set duty to half of period
+    PWM6DC = (DUTY * 4 * (T2PRbits.PR + 1) / 100)<<6 ;       // Set duty to half of period
     PIR4bits.TMR2IF = 0;            // clear int flag 
     T2CLKCONbits.CS = 0b0001;       // Fosc/4
     T2CONbits.CKPS = 0;             // Prescaler of 0 
     T2CONbits.T2ON = 1;             // Turn on timer 
         
     TRISBbits.TRISB6 = 0;           // set RB6 as output 
-    PWM7CONbits.EN = 1;             // enable PWM 
+    PWM6CONbits.EN = 1;             // enable PWM 
     return;
     
 }
